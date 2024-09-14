28ce1f03f229144e3d760b0f2793073373d2833c
kadircali <a.kadircali@hotmail.com>
2024-09-14 22:07:12 +0300

    Alisveris_sepeti
    güncelleme

--- sepet/src/Test/Test.java
+++ sepet/src/Test/Test.java
@@ -1,10 +1,6 @@
 package Test;
 
-import java.util.Map;
 import java.util.Scanner;
-
-
-
 import sepet.Sepet;
 
 public class Test {
@@ -20,77 +16,82 @@ public class Test {
 		 */
 
 		Scanner scan = new Scanner(System.in);
-		Sepet sepet = new Sepet();
-		int secim = 0;
+		
+		
+		Sepet<String, Integer> sepet = new Sepet<>();
 		sepet.sepetOlustur();
-		System.out.println("vektörel market");
+
+		System.out.println("Vektörel Market");
 
 		while (true) {
 			secim();
-			System.out.print("lütfen seçiminizi yapın:");
-            int secim1 = scan.nextInt();
-            
-            switch (secim1) {
-                case 1:
-                    gidaSecim();
-                    String[] gidaUrunleri = {"çikolata", "süt", "ekmek", "peynir", "zeytin"};
-                    secimVeEkle(sepet, gidaUrunleri);
-                    break;
-                case 2:
-                    temizlikSecim();
-                    String[] temizlikUrunleri = {"bulaşık deterjanı", "çamaşır suyu", "sıvı sabun", "tuvalet kağıdı", "yer temizleyici"};
-                    secimVeEkle(sepet, temizlikUrunleri);
-                    break;
-                case 3:
-                    elektronikSecim();
-                    String[] elektronikUrunleri = {"kulaklık", "akıllı saat", "telefon", "televizyon", "laptop"};
-                    secimVeEkle(sepet, elektronikUrunleri);
-                    break;
-                case 4:
-                    // sepet görüntüle
-                    sepet.sepetGoruntule();
-                    break;
-                case 5:
-                    // çıkış
-                    return;
-                default:
-                    System.out.println("hatalı seçim");
-                    break;
-            }
-		}
+			System.out.print("Lütfen seçiminizi yapın: ");
+			int secim1 = scan.nextInt();
 
+			if (secim1 == 1) {
+				gidaSecim();
+				String[] gidaUrunleri = { "çikolata", "süt", "ekmek", "peynir", "zeytin" };
+				secimVeEkle(sepet, gidaUrunleri);
+			} else if (secim1 == 2) {
+				temizlikSecim();
+				String[] temizlikUrunleri = { "bulaşık deterjanı", "çamaşır suyu", "sıvı sabun", "tuvalet kağıdı",
+						"yer temizleyici" };
+				secimVeEkle(sepet, temizlikUrunleri);
+			} else if (secim1 == 3) {
+				elektronikSecim();
+				String[] elektronikUrunleri = { "kulaklık", "akıllı saat", "telefon", "televizyon", "laptop" };
+				secimVeEkle(sepet, elektronikUrunleri);
+			} else if (secim1 == 4) {
+				// Sepet görüntüle
+				sepet.sepetGoruntule();
+//                System.out.print("Fiş yazdır:?(e-h) ");
+//                scan.nextLine(); //
+				// String fis = scan.nextLine();
+				// dosyaya yazmayı öğrendikten sonra güncelle
+//                if (fis.equalsIgnoreCase("e")) {
+//                    System.out.println("Fişiniz yazdırıldı.");
+//                } else {
+//                    System.out.println("Fiş yazdırılmadı.");
+//                }
+			} else if (secim1 == 5) {
+				// Çıkış
+				System.out.println("Çıkış yapılıyor...");
+				break;
+			} else {
+				System.out.println("Hatalı seçim");
+			}
+		}
 	}
 
 	public static void secim() {
-		System.out.println("1-Gıda\n2-Temizlik\n3-Elektronik\n4-sepet görüntüle\n5-çıkış");
+		System.out.println("1-Gıda\n2-Temizlik\n3-Elektronik\n4-Sepet Görüntüle\n5-Çıkış");
 	}
 
 	public static void elektronikSecim() {
-		System.out.println("1-kulaklık\n2-akıllı saat\n3-telefon\n4-televizyon\n5-laptop");
+		System.out.println("1-Kulaklık\n2-Akıllı Saat\n3-Telefon\n4-Televizyon\n5-Laptop");
 	}
 
 	public static void gidaSecim() {
-		System.out.println("1- Çikolata\n2- Süt\n3- Ekmek\n4- Peynir\n5- Zeytin");
+		System.out.println("1-Çikolata\n2-Süt\n3-Ekmek\n4-Peynir\n5-Zeytin");
 	}
 
 	public static void temizlikSecim() {
-		System.out
-				.println("1- Bulaşık Deterjanı\n2- Çamaşır Suyu\n3- Sıvı Sabun\n4- Tuvalet Kağıdı\n5- Yer Temizleyici");
+		System.out.println("1-Bulaşık Deterjanı\n2-Çamaşır Suyu\n3-Sıvı Sabun\n4-Tuvalet Kağıdı\n5-Yer Temizleyici");
 	}
-	
-	public static void secimVeEkle(Sepet sepet, String[] urunler) {
-	    Scanner scan = new Scanner(System.in);
-	    int secim1, adet;
-	    
-	    System.out.print("lütfen seçiminizi yapın:");
-	    secim1 = scan.nextInt();
-	    System.out.print("lütfen adet giriniz:");
-	    adet = scan.nextInt();
 
-	    if (secim1 > 0 && secim1 <= urunler.length) {
-	        sepet.sepeteEkle(urunler[secim1 - 1], adet);
-	    } else {
-	        System.out.println("hatalı seçim");
-	    }
+	public static void secimVeEkle(Sepet<String, Integer> sepet, String[] urunler) {
+		Scanner scan = new Scanner(System.in);
+		int secim1, adet;
+
+		System.out.print("Lütfen seçiminizi yapın: ");
+		secim1 = scan.nextInt();
+		System.out.print("Lütfen adet giriniz: ");
+		adet = scan.nextInt();
+
+		if (secim1 > 0 && secim1 <= urunler.length) {
+			sepet.sepeteEkle(urunler[secim1 - 1], adet);
+		} else {
+			System.out.println("Hatalı seçim");
+		}
 	}
 }
--- sepet/src/sepet/Sepet.java
+++ sepet/src/sepet/Sepet.java
@@ -3,93 +3,95 @@ package sepet;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Sepet {
+public class Sepet<K, V extends Number> {
 
-	// ad,adet
-	Map<String, Integer> sepet;
+    // ad, adet
+    Map<K, V> sepet;
 
-	public void sepetOlustur() {
-		this.sepet = new HashMap<String, Integer>();
-	}
+    public void sepetOlustur() {
+        this.sepet = new HashMap<>();
+    }
 
-	public void sepeteEkle(String key, int value) {
-		// sepette var mı ?
-		if (sepet.containsKey(key)) {
-			int mevcutMiktar = sepet.get(key);
-			sepet.put(key, mevcutMiktar + value);
-		} else {
-			sepet.put(key, value);
-		}
+    public void sepeteEkle(K key, V value) {
+        // sepette var mı?
+        if (sepet.containsKey(key)) {
+            V mevcutMiktar = sepet.get(key);
+            sepet.put(key, (V) (Integer.valueOf(mevcutMiktar.intValue() + value.intValue())));
+        } else {
+            sepet.put(key, value);
+        }
+    }
 
-	}
+    public void sepettenCikart(K key) {
+        if (sepet.containsKey(key)) {
+            sepet.remove(key);
+        } else {
+            System.out.println("Ürün bulunamadı");
+        }
+    }
 
-	public void sepettenCikart(String key) {
-		if (sepet.containsKey(key)) {
-			sepet.remove(key);
-		} else {
-			System.out.println("ürün bulunamadı");
-		}
-	}
+    public void sepetGoruntule() {
+        if (sepet.isEmpty()) {
+            System.out.println("Sepetiniz boş!");
+        } else {
+            System.out.println("Sepetinizdeki ürünler:");
+            int toplamTutar = 0;
 
-	public void sepetGoruntule() {
-		if (sepet.isEmpty()) {
-			System.out.println("Sepetiniz boş!");
-		} else {
-			System.out.println("Sepetinizdeki ürünler:");
-			int toplamTutar = 0;
+            for (Map.Entry<K, V> entry : sepet.entrySet()) {
+                K urunAdi = entry.getKey();
+                V urunAdedi = entry.getValue();
 
-			for (Map.Entry<String, Integer> entry : sepet.entrySet()) {
-				String urunAdi = entry.getKey();
-				int urunAdedi = entry.getValue();
+                int birimFiyat = getBirimFiyat(urunAdi);
+                int urunToplamFiyati = birimFiyat * urunAdedi.intValue();
 
-				int birimFiyat = getBirimFiyat(urunAdi);
-				int urunToplamFiyati = birimFiyat * urunAdedi;
+                System.out.println(urunAdi + " - " + urunAdedi + " adet, Birim Fiyat: " + birimFiyat + " TL, Toplam: "
+                        + urunToplamFiyati + " TL");
 
-				System.out.println(urunAdi + " - " + urunAdedi + " adet, Birim Fiyat: " + birimFiyat + " TL, Toplam: "
-						+ urunToplamFiyati + " TL");
+                toplamTutar += urunToplamFiyati;
+            }
 
-				toplamTutar += urunToplamFiyati;
-			}
+            System.out.println("Toplam Tutar: " + toplamTutar + " TL");
+        }
+    }
 
-			System.out.println("Toplam Tutar: " + toplamTutar + " TL");
-		}
-	}
-
-	private int getBirimFiyat(String urunAdi) {
-		switch (urunAdi) {
-		case "çikolata":
-			return 10;
-		case "süt":
-			return 25;
-		case "ekmek":
-			return 10;
-		case "peynir":
-			return 200;
-		case "zeytin":
-			return 250;
-		case "bulaşık deterjanı":
-			return 20;
-		case "çamaşır suyu":
-			return 15;
-		case "sıvı sabun":
-			return 12;
-		case "tuvalet kağıdı":
-			return 45;
-		case "yer temizleyici":
-			return 22;
-		case "kulaklık":
-			return 100;
-		case "akıllı saat":
-			return 500;
-		case "telefon":
-			return 3000;
-		case "televizyon":
-			return 5000;
-		case "laptop":
-			return 7500;
-		default:
-			return 0;
-		}
-	}
-
+    private int getBirimFiyat(K urunAdi) {
+        
+        if (urunAdi instanceof String) {
+            switch ((String) urunAdi) {
+                case "çikolata":
+                    return 10;
+                case "süt":
+                    return 25;
+                case "ekmek":
+                    return 10;
+                case "peynir":
+                    return 200;
+                case "zeytin":
+                    return 250;
+                case "bulaşık deterjanı":
+                    return 20;
+                case "çamaşır suyu":
+                    return 15;
+                case "sıvı sabun":
+                    return 12;
+                case "tuvalet kağıdı":
+                    return 45;
+                case "yer temizleyici":
+                    return 22;
+                case "kulaklık":
+                    return 100;
+                case "akıllı saat":
+                    return 500;
+                case "telefon":
+                    return 3000;
+                case "televizyon":
+                    return 5000;
+                case "laptop":
+                    return 7500;
+                default:
+                    return 0;
+            }
+        }
+        return 0; 
+    }
 }
